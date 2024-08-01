fd523febb8a6c51b7591fdd1d43303970a113b73
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-08-01 22:04:57 +0530

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
+import Header from './Components/Header'
+
+export default function App() {
+  return (
+   <BrowserRouter>
+      <Header/>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/sign-in" element={<Signin/>} />
+          <Route path="/sign-up" element={<Signup/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/profile" element={<Profile/>} />
+      </Routes>
+   </BrowserRouter>
+  )
+}
