685423ff0f3566af0ed19b3c97e6b7746a090c59
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-08-03 21:44:23 +0530

    Little changes in code due to auto save

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -44,10 +44,9 @@ export const deleteUser = async (req, res, next) => {
 
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('access_token');
+    res.clearCookie("access_token");
     res.status(200).json("User has been successfully deleted!");
   } catch (error) {
     next(error);
   }
 };
-
