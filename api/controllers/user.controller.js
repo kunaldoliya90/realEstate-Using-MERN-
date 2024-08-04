46b6ba3db8ac4407e5710519919d603da85d44c8
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-08-04 22:28:18 +0530

    API route to get user listings

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
@@ -38,7 +39,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id) {
+  if (req.user.id === req.params.id) {
     return next(errorHandler(401, "You can only delete your own account!"));
   }
 
@@ -48,5 +49,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User has been successfully deleted!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
